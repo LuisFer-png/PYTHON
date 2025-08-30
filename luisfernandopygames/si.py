3e63feeea125c7be6680ed739cdf7b3003c8ca15
LuisFer-png <dondestalangosteno@gmail.com>
2025-08-30 17:52:18 -0500

    Add files via upload

--- luisfernandopygames/si.py
+++ luisfernandopygames/si.py
@@ -9,11 +9,38 @@ Snake_COLOR = (43,52,24)
 cell_size = 30
 number_of_cells = 25
 
+class Food:
+    def __init__(self):
+        self.position = self.generate_random_pos()
+
+    def draw(self):
+        food_rect = pygame.Rect(self.position.x * cell_size, self.position.y * cell_size,cell_size,cell_size)
+        pygame.draw.rect(screen, Snake_COLOR, food_rect)
+
+    def generate_random_pos(self):
+        x = random.randint(0, number_of_cells - 1)
+        y = random.randint(0, number_of_cells - 1)
+        position = Vector2(x, y)
+        return position   
+    
+class Snake:
+    def __init__(self):
+        self.body = [Vector2(6,9), Vector2(5,9), Vector2(4,9)]
+
+    def draw(self):
+        for segment in self.body:
+            segment_rect = pygame.Rect(segment.x * cell_size, segment.y * cell_size, cell_size, cell_size)
+            pygame.draw.rect(screen, Snake_COLOR, segment_rect)
+
 screen = pygame.display.set_mode((cell_size * number_of_cells,cell_size * number_of_cells))
 
 pygame.display.set_caption("Retro Snake")
 
 clock = pygame.time.Clock()
+
+food = Food()
+
+Snake = Snake()
 
 while True:
     for event in pygame.event.get():
@@ -21,6 +48,8 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill(BG_COLOR)
+    food.draw()
+    Snake.draw()
 
     pygame.display.update()
     clock.tick(60)
